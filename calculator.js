10f30adfa4b624b9c88933d8b55dc644afb214a6
Viper1007 <56436023+Viper1007@users.noreply.github.com>
2019-11-02 22:16:54 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,8 @@
+function display(val){
+    alert("Pop up Message");
+}
+function solve(){
+   var x=  document.getElementById("result").value;
+   var y=eval(x);
+   document.getElementById("result").value=y
+}
